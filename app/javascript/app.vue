3c2e8d07038632db0fdbfe75d1238fa54a714736
Makotox0207 <xiaoxishicheng@gmail.com>
2021-06-13 19:11:06 +0900

    メモ一覧機能

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <!-- memosを表示する -->
+    <ul>
+      <li v-for="memo in memos" :key="memo.id">
+        title:{{memo.title}}, description{{memo.description}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "これはメモアプリのテストです"
+      memos: []
+    }
+  },
+  // アプリが立ち上がった最初のタイミングで実行される関数を定義
+  mounted(){
+    this.setMemo();
+  },
+  // axiosでapi/memosのAPIを呼び出す
+  methods: {
+    setMemo: function(){
+      axios.get('/api/memos')
+      .then( response => {
+        this.memos = response.data;
+      })
     }
   }
 }
